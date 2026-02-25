93aae8835f5e8d65ebc362956f51d034a152241c
visvesvaran <visvesvaran62@gmail.com>
2026-02-25 23:16:14 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,80 @@
+// 1.java script:
+
+// java script is high level programming language and its used to create a web page interactive and Dynamic pages.
+
+// 2.variable:
+// a variable is container used to store a data values.
+
+// 3.let-we can redeclare and reassigning the value.
+// var-redeclare not allowed but we can reasign.
+// const- both  are not allowed redeclare and reasign.
+
+// 4.data type:
+// A data type defines the type of value a variable can store.
+// example:
+// String,boolean,Number,null
+
+// 5. types of primitive datatypes:
+// String,number,boolean,null,undefined
+
+
+//  6.non-primitive datatypes:
+//  Object,array,function
+
+// 7.operator:
+// Operators are special symbols used to perform operations on values and variables.
+
+
+// 8.type of operator:
+// ->arithemetic operator
+// ->assignment operator
+// ->Logical operator
+// ->Compression operator
+
+// 9.Expression:
+
+// An expression is any valid piece of code that produces, it returns a value.
+
+// 10.What is statement?
+      
+// A statement in JavaScript is a complete instruction that tells the JavaScript engine to perform an action.
+
+// 11.What is control statement?
+   
+//  A control statement in JavaScript is a statement that controls the flow of execution of a program.
+
+//  12.What are conditional statements:
+    
+//      Conditional statements are control statements in JavaScript that execute different blocks of code based on whether a condition is true or false.
+
+//      Type:
+//      if,
+//      if else,
+//      switch.
+ 
+// 13.What are looping statements?
+    
+//      Looping statements in JavaScript are control statements that repeatedly execute a block of code as long as a specified condition is true.
+//      types of looping statement:
+//      for loop,
+//      while loop,
+//      do while.
+//      for in loop,
+//      for of loop.
+
+// 14. What is hoisting?   
+//      Hoisting in JavaScript is a behavior where variable and function declarations are moved to the top of their scope during the memory creation phase, before the code is executed. 
+  
+// 15.scope:
+//   Scope in JavaScript refers to the area or context in which a variable or function is accessible.
+//   types:
+//   global scope,
+//   local scope,
+//   block scope.
+
+// 16.What is closure?
+//    Closure in JavaScript is a function that remembers and can access variables from its outer (enclosing) function even after that outer function has finished executing.  
+
+ 
+
+     
